5e23bcc965f4025d8a1ecb48b0c9911dcb80e9a5
Jonah Odongo <126646855+o-jonah@users.noreply.github.com>
2025-01-14 12:00:50 +0300

    Create events handler

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,24 @@
+import EventForm from "@/components/shared/EventForm"
+import { auth } from "@clerk/nextjs/server";
+
+
+const CreateEvent = async () => {
+  const { sessionClaims } = await auth();
+
+  const userId = sessionClaims?.userId as string;
+
+  return (
+    <> 
+    <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center 
+    py-5 md:py-10">
+      <h3 className="wrapper h3-bold text-center sm:text-left">Create event</h3>
+    </section>
+
+    <div className="wrapper">
+      <EventForm userId={userId} type="Create"/>
+    </div>
+    </>
+  )
+}
+
+export default CreateEvent
